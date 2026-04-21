e8ec8da49e22c53a7d58a975875214543c39ce78
ankityadav3216 <ankit306600@gmail.com>
2026-04-21 23:47:50 +0530

    comment index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 
-declare(strict_types=1);
+// declare(strict_types=1);
 
-header('Content-Type: text/html; charset=UTF-8');
+// header('Content-Type: text/html; charset=UTF-8');
 
-$homepage = __DIR__ . '/homepage/index/index.html';
+// $homepage = __DIR__ . 'planetG/homepage/index/index.html';
 
-if (is_file($homepage)) {
-    readfile($homepage);
-    exit;
-}
+// if (is_file($homepage)) {
+//     readfile($homepage);
+//     exit;
+// }
 
-http_response_code(404);
-echo 'Homepage entry file not found.';
+// http_response_code(404);
+// echo 'Homepage entry file not found.';
